c946e172beeac5173fbbf394c852dac11bfe6d4b
Luam <luam13@gmail.com>
2016-02-16 01:54:07 -0500

    clean up and added contact area

--- js/myJQuery.js
+++ js/myJQuery.js
@@ -1,10 +1,16 @@
 $(function() {
+  
   $(".morph-content").hide();
 
-$( ".watch-now-btn" ).click(function() {
-		$(".morph-content").slideToggle();
-});
+	$( ".watch-now-btn" ).click(function() {
+			$(".morph-content").slideToggle();
+	});
 
 
+	  $(".morph-content-2").hide();
+
+		$( ".message-btn" ).click(function() {
+				$(".morph-content-2").slideToggle();
+		});
 
 });
